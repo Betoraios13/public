fbbd5bede877d434fc00b3adfa71b6c10bb2f15f
Betoraios13 <89354031+Betoraios13@users.noreply.github.com>
2024-05-05 10:29:09 -0300

    Add files via upload

---
+++ PageJS.js
@@ -0,0 +1,22 @@
+
+
+function OpenNewtab (url){
+    const win = window.open(url, "_blank")
+    win.focus();
+}
+
+function CloseTab(){
+    const win = window.close()
+}
+
+function OpenSingleton(){
+    OpenNewtab("singleton.html");
+}
+
+function OpenInterface(){
+    OpenNewtab("interface.html");
+}
+
+function OpenNameSpace(){
+    OpenNewtab("namespace.html");
+}
